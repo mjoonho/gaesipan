f36e0f156490f81282ddc81acad9c8f51a61921e
mjoonho <103565908+mjoonho@users.noreply.github.com>
2022-06-03 17:50:48 +0900

    댓글 전부 불러오고 필터 걸러서 보여주는 형식에서 필요한 댓글만 가져오는 형식으로 바꿈 이젠 게시글을 삭제 하면 달려있던 댓글들고 같이 삭제되는거 구현하면 댓글 부분은 완벽 할듯

--- src/main/java/com/gaesipan/gaesipan/domain/comment/CommentRepository.java
+++ src/main/java/com/gaesipan/gaesipan/domain/comment/CommentRepository.java
@@ -7,5 +7,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByOrderByModifiedAtDesc();
 
+
     List<Comment> findByIdOrderByModifiedAtDesc(long id);
 }
